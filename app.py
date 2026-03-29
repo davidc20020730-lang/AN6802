42f64d91109a4a4721e3ab4ddfb3dd1c96e29b92
unikonta <72618104+unikonta@users.noreply.github.com>
2026-03-29 07:50:07 +0000

    1

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import os
 from groq import Groq
 
 
-#os.environ["GROQ_API_KEY"] = ""
+# os.environ["GROQ_API_KEY"] = ""
 
 model = joblib.load("foodexp.pkl")
 
@@ -73,6 +73,10 @@ def groqReply():
         ]
     )
     return(render_template("groqReply.html",r=r.choices[0].message.content))
+
+@app.route("/equity", methods=["get", "post"])
+def equity():
+    return(render_template("equity.html"))
 
 if __name__ == "__main__":
     app.run()
